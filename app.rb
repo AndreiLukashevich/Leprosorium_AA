d0822d96c681840d599ffea9847fe507c59a886c
AndreiLukashevich <rid7003482@gmail.com>
2016-11-01 18:45:40 +0300

    Add parameter validation

--- app.rb
+++ app.rb
@@ -12,7 +12,6 @@ end
 # before вызывается каждый раз при перезагрузке любой страницы
 before do
 	# инициализация БД
-
 	init_db
 end
 
@@ -48,5 +47,11 @@ end
 post '/new' do
 	# получаем переменную из post-запроса
 	content = params[:content]
+
+	if content.length <= 0
+		@error = 'Type post text'
+		return  erb :new
+	end
+
 	erb "You typed #{content}"
 end
